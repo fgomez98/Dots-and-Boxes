2204f1121fe5a08aed28fd568e1653bc93a50d45
Pedro Remigio Pingarilho <premigio@itba.edu.ar>
2018-06-16 16:44:21 -0300

    toma

--- src/com/company/Model/Board.java
+++ src/com/company/Model/Board.java
@@ -4,13 +4,21 @@ public class Board {
     private Integer[][] boxBoard;
     private Integer[][] boxHorizontal;
     private Integer[][] boxVertical;
+    private int player1Score, player2Score;
 
     public Board(int N) {
         boxBoard = new Integer[N-1][N-1];
         boxHorizontal = new Integer[N][N-1];
         boxVertical = new Integer[N-1][N];
+        player1Score = 0;
+        player2Score = 0;
     }
 
+    public boolean setVEdge(int x, int y, int player){
+        return false;
+    }
 
-
+    public boolean setHEdge(int x, int y, int player){
+        return false;
+    }
 }
