3a6857c3758465c654ce904e9bb743a72052afbe
Pedro Remigio Pingarilho <premigio@itba.edu.ar>
2018-06-16 17:48:14 -0300

    eee

--- src/com/company/Model/Board.java
+++ src/com/company/Model/Board.java
@@ -3,7 +3,7 @@ package com.company.Model;
 public class Board {
 
     private final static int ARC = 1;
-    private final static int NOARC = 2;
+    private final static int NOARC = 0;
     private final static int PLAYER1 = 1;
     private final static int PLAYER2 = 2;
     private Integer[][] boxBoard;
@@ -12,23 +12,62 @@ public class Board {
     private int player1Score, player2Score;
     private int N;
 
-    public Board(int N) {
+    public Board(int n) {
+        N = n;
         boxBoard = new Integer[N-1][N-1];
-        boxHorizontal = new Integer[N][N-1];
-        boxVertical = new Integer[N-1][N];
+        boxHorizontal = new Integer[N-1][N];
+        boxVertical = new Integer[N][N-1];
         player1Score = 0;
         player2Score = 0;
-
-
-    public boolean setVEdge(int x, int y, int player){
-        return false;
     }
 
     public boolean setHEdge(int x, int y, int player){
+        if (boxHorizontal[y][x] == NOARC){
+            boxHorizontal[y][x] = player;
+            if(y < N && boxHorizontal[y+1][x] == ARC && boxVertical[y][x] == ARC && boxVertical[y][x+1] == ARC){
+                if(player == 1){
+                    player1Score++;
+                }else{
+                    player2Score++;
+                }
+                boxBoard[y][x] = player;
+            }
+            if(y > 0 && boxHorizontal[y-1][x] == ARC && boxVertical[y-1][x] == ARC && boxVertical[y-1][x+1] == ARC){
+                if(player == 1){
+                    player1Score++;
+                }else{
+                    player2Score++;
+                }
+                boxBoard[][] = player;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setVEdge(int x, int y, int player){
+        if(boxVertical[y][x] == NOARC){
+            boxVertical[y][x] = player;
+            if( x < N && boxVertical[y][x+1] == ARC && boxHorizontal[y][x]== ARC && boxHorizontal[y-1][x] == ARC){
+                if(player == 1){
+                    player1Score++;
+                }else{
+                    player2Score++;
+                }
+                boxBoard[y][x] = player;
+            }if(x > 0 && boxVertical[y][x-1] == ARC && boxHorizontal[y-1][x-1] == ARC && boxHorizontal[y][x-1] == ARC){
+                if(player == 1){
+                    player1Score++;
+                }else{
+                    player2Score++;
+                }
+                boxBoard[y][x-1] = player;
+            return true;
+        }
         return false;
     }
 
     public boolean fillBoards(int n) {
-
+        return false;
     }
 }
