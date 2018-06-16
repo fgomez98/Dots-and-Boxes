c9473c41f8d165d9f29490a8ec6cd26f9cb938ae
Unknown <fgomez@itba.edu.ar>
2018-06-16 16:50:34 -0300

    Auto stash before merge of "master" and "origin/master"

--- src/com/company/Model/Board.java
+++ src/com/company/Model/Board.java
@@ -1,10 +1,16 @@
 package com.company.Model;
 
 public class Board {
+
+    private final static int ARC = 1;
+    private final static int NOARC = 2;
+    private final static int PLAYER1 = 1;
+    private final static int PLAYER2 = 2;
     private Integer[][] boxBoard;
     private Integer[][] boxHorizontal;
     private Integer[][] boxVertical;
     private int player1Score, player2Score;
+    private int N;
 
     public Board(int N) {
         boxBoard = new Integer[N-1][N-1];
@@ -12,7 +18,7 @@ public class Board {
         boxVertical = new Integer[N-1][N];
         player1Score = 0;
         player2Score = 0;
-    }
+
 
     public boolean setVEdge(int x, int y, int player){
         return false;
@@ -20,5 +26,9 @@ public class Board {
 
     public boolean setHEdge(int x, int y, int player){
         return false;
+    }
+
+    public boolean fillBoards(int n) {
+
     }
 }
