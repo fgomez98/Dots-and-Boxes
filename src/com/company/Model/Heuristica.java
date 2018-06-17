7caa7ab3c1134528c4f3c25243ae56708cffa772
Unknown <fgomez@itba.edu.ar>
2018-06-17 18:20:57 -0300

    .

--- src/com/company/Model/Heuristica.java
+++ src/com/company/Model/Heuristica.java
@@ -1,5 +1,5 @@
 package com.company.Model;
 
 public interface Heuristica {
-    int evaluate(Board board, int player);
+    int evaluate(Board board);
 }
--- src/com/company/Model/MiniMax.java
+++ src/com/company/Model/MiniMax.java
@@ -1,15 +1,17 @@
 package com.company.Model;
 
+import org.omg.PortableInterceptor.INACTIVE;
+
 public class MiniMax implements Heuristica {
 
     @Override
-    public int evaluate(Board board, int player) {
+    public int evaluate(Board board) {
         return 0;
     }
 
     public int minimax(Tree current, int depth, boolean maximaizer, int player, int alpha, int beta) {
         if (depth == 0 || current.board.boardComplete()) {
-            return evaluate(current.board, player);
+            return evaluate(current.board);
         }
         if (maximaizer) { //MAXIMIZER
             int maxEvaluation = Integer.MIN_VALUE;
@@ -37,6 +39,21 @@ public class MiniMax implements Heuristica {
             }
             return minEvaluation;
         }
+    }
+
+    public Board bestMove(Board board, int depth, int player) {
+        Tree tree = new Tree(board);
+        Board bestBoard = null;
+        tree.generateChildren();
+        int bestBoardEvaluation = Integer.MIN_VALUE;
+        int auxEvaluation = 0;
+        for (Tree child : tree.children) {
+            if ((auxEvaluation = minimax(child, depth-1, false, -player, Integer.MIN_VALUE, Integer.MAX_VALUE)) > bestBoardEvaluation) {
+                bestBoardEvaluation = auxEvaluation;
+                bestBoard = child.board;
+            }
+        }
+        return bestBoard;
     }
 
 }
