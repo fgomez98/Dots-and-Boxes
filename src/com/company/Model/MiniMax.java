ec5b293b1f56ca63b216ee6e4009a75c8eb687e8
Unknown <fgomez@itba.edu.ar>
2018-06-17 18:02:52 -0300

    .

--- src/com/company/Model/MiniMax.java
+++ src/com/company/Model/MiniMax.java
@@ -3,20 +3,24 @@ package com.company.Model;
 public class MiniMax implements Heuristica {
 
     @Override
-    public int evaluate(Board board) {
+    public int evaluate(Board board, int player) {
         return 0;
     }
 
-    public int minimax(Tree current, int depth, boolean maximaizer, int player) {
+    public int minimax(Tree current, int depth, boolean maximaizer, int player, int alpha, int beta) {
         if (depth == 0 || current.board.boardComplete()) {
-            return evaluate(current.board);
+            return evaluate(current.board, player);
         }
         if (maximaizer) { //MAXIMIZER
             int maxEvaluation = Integer.MIN_VALUE;
             current.generateChildren();
             for (Tree child : current.children) {
-                int auxEvalution = minimax(child, depth-1, !maximaizer, -player); {
+                int auxEvalution = minimax(child, depth-1, !maximaizer, -player, alpha, beta); {
                     maxEvaluation = Math.max(maxEvaluation, auxEvalution);
+                    alpha = Math.max(alpha, auxEvalution);
+                    if (beta <=  alpha) {
+                        return maxEvaluation;
+                    }
                 }
             }
             return maxEvaluation;
@@ -24,8 +28,12 @@ public class MiniMax implements Heuristica {
             int minEvaluation = Integer.MAX_VALUE;
             current.generateChildren();
             for (Tree child : current.children) {
-                int auxEvaluaation = minimax(child, depth-1, !maximaizer, -player);
+                int auxEvaluaation = minimax(child, depth-1, !maximaizer, -player, alpha, beta);
                 minEvaluation = Math.min(minEvaluation, auxEvaluaation);
+                beta = Math.min(beta, auxEvaluaation);
+                if (beta <= alpha) {
+                    return minEvaluation;
+                }
             }
             return minEvaluation;
         }
